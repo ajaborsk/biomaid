6169df1fc57be4b19d70875bd064e9975e13279b
alexandre <alexandre.jaborska@laposte.net>
2022-12-11 09:52:52 +0100

    Added custom 'like' lookup for Django ORM

--- common/apps.py
+++ common/apps.py
@@ -25,6 +25,8 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Fieldset, Div, Button
 from django.apps import AppConfig, apps
 from django.db.models import Q
+from django.db.models import Lookup
+from django.db.models.fields import Field
 from django.forms import Form
 from django.urls import reverse
 from django.utils.timezone import now
@@ -36,6 +38,17 @@ from smart_view.layout import SmartLayoutFieldset, SmartLayoutField
 from smart_view.smart_form import BaseSmartForm
 
 logger = logging.getLogger(__name__)
+
+
+@Field.register_lookup
+class Like(Lookup):
+    lookup_name = 'like'
+
+    def as_sql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return '%s LIKE %s' % (lhs, rhs), params
 
 
 def user_settings_level(level_id, categories, user_settings):
