a331464b0ba4f330ef854d82faff71eba7d4840b
alexandre <alexandre.jaborska@laposte.net>
2023-01-07 16:41:35 +0100

    Updates launch script again

--- run-instance.sh
+++ run-instance.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Generate documentation (if needed)
-#...
+# ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/python runserver make_docs
 
 # Deploy statics
-#...
+~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/python runserver collectstatic
 
 # Run gunicorn workers
-~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/gunicorn --workers 4 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
+~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/gunicorn --workers 4 --log-file ../log/gunicorn_django.log -b unix:/run/biomaid/$USER.sock dra.wsgi
