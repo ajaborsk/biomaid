f9e310fc76d8c7e09977a30b30b11263aa85489d
alexandre <alexandre.jaborska@laposte.net>
2023-02-17 21:04:04 +0100

    Reduce workers for gunicorn

--- run-instance.sh
+++ run-instance.sh
@@ -7,4 +7,4 @@
 ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/python manage.py collectstatic --no-input
 
 # Run gunicorn workers
-exec ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/gunicorn --workers 4 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
+exec ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/gunicorn --workers 2 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
