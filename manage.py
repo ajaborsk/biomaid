92c6bad264e17c75366db4389d1e4e1cbe8cac4d
alexandre <alexandre.jaborska@laposte.net>
2023-06-01 22:53:35 +0200

    Clean manage.py with time travel

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import time_machine
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
@@ -18,4 +20,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # Small hack to allow easy time travel (used by mktest.py script)
+    if os.environ.get('TIME_TRAVEL'):
+        # idx = sys.argv.index('--timetravel')
+        # sys.argv.pop(idx)  # get the option name
+        time_destination = os.environ.get('TIME_TRAVEL')  # get the destination (as a date string)
+        print("travelling into time at", time_destination)
+        with time_machine.travel(time_destination):
+            main()
+    else:
+        # print("run without time travelling.")
+        main()
---
+++ tools/mktest.py
@@ -0,0 +1,201 @@
+from gettext import gettext as _
+from argparse import ArgumentParser
+import time
+from cmd import Cmd
+from subprocess import run, Popen, STDOUT, DEVNULL, PIPE
+from signal import SIGINT
+import os
+
+from playwright.sync_api import sync_playwright
+
+
+def biomaid_launch(args):
+    # TODO: Check for errors instead of throwing stderr to /dev/null...
+    # TODO: Check virtual environment ?
+    # TODO: parametric host:port
+    # TODO: parametric time travel destination
+    # ensure we use the mktest database
+    os.environ['MKTEST'] = '1'
+    # make Django believe we are in the past :-) (needs a hacked manage.py)
+    os.environ['TIME_TRAVEL'] = '2020-08-15 03:00:00'
+    # TODO: Check database ? How ?
+    print("Empty database...")
+    run(['python', 'manage.py', 'reset_db', '--no-input'], stdout=DEVNULL, stderr=STDOUT)
+    print("Migrate...")
+    run(['python', 'manage.py', 'migrate'], stdout=DEVNULL, stderr=STDOUT)
+    print("Load tests DB...")
+    run(['python', 'manage.py', 'loaddata', 'fixtures/tests_db.json'], stdout=DEVNULL, stderr=STDOUT)
+    print("Launch Django server...")
+    # pipe stdout to a pipe so we can monitor its behaviour
+    server = Popen(
+        ['python', 'manage.py', 'runserver', '--insecure', 'localhost:8123'],
+        stdout=PIPE,
+        stderr=STDOUT,
+        encoding='UTF-8',
+    )
+
+    stdout = ''
+    launched = False
+    while not launched:
+        data = server.stdout.readline()
+        stdout += data
+        if "Quit the server with CONTROL-C" in data:
+            time.sleep(1)
+            launched = True
+        if server.poll() is not None:
+            print(stdout)
+            break
+    if launched:
+        return server
+
+
+class Cli(Cmd):
+    prompt = 'BiomAid MkTest CLI > '
+    intro = _("Do whatever you want !\nType 'help' to get some help :-)")
+
+    def __init__(self, args):
+        self.args = args
+        super().__init__()
+        self.registred_users = {
+            'tomiela': {
+                'name': "Lana Tomie",
+                'description': "Experte métier sur l'UF 1001 (USLD), dispatcheuse campagne équipements TEST",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'root': {
+                'name': "Super utilisateur",
+                'description': "Super utilisateur",
+                'password': 'introuvable',
+            },
+            'enbaveyv': {
+                'name': "Yvon Enbaver",
+                'description': "",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'deboziel': {
+                'name': "Ella de Bozieux",
+                'description': "Directrice adjointe UF 0001 et 0002 et Directrice référente UF 0003",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'cekilesy': {
+                'name': "Sylvie Cekilépamor",
+                'description': "Cheffe de pôle UF 0003",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'couranth': {
+                'name': "Théo Courant",
+                'description': "Cadre de santé UF 0003",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'timettvi': {
+                'name': "Vincent Timettre",
+                'description': "Expert métier sur les UF 0001, 0002 et 0003",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'bonbeuje': {
+                'name': "Jean Bonbeurre",
+                'description': "Expert métier travaux sur l'établissement support ETS",
+                'password': "yQ6FfiKypa7h8Hc",
+            },
+            'zonloilo': {
+                'name': "Laurie Zonlointain",
+                'description': "Ingénieure travaux sur l'établissement support ETS, dispatcheur campagne travaux TVXCT",
+                'password': "yQ6FfiKypa7h8Hc",
+            },
+            'arbitre_biomed': {
+                'name': "Harry Staukrate",
+                'description': "Arbitre programme BIO-00-PE",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+            'igonnepa': {
+                'name': "Paul Igonne",
+                'description': "Arbitre programme TVX-00-PC",
+                'password': 'yQ6FfiKypa7h8Hc',
+            },
+        }
+        self.server = biomaid_launch(args)
+        if self.server:
+            print("Do something with ready server...")
+
+    def do_long_help(self, line):
+        "Tada !"
+        if 'help' in line:
+            print(f"{line=}")
+        else:
+            print(
+                _(
+                    "Recognized commands :\n"
+                    " help: Display this help\n"
+                    " quit: Quit this CLI without saving anything\n"
+                    " save: Save the tests database into fixtures/tests_db.json file\n"
+                    " browse_as *user*: Open a web browser on the tests server then connect as *user*. Possibles users are :\n{users:s}"
+                ).format(
+                    users="".join(
+                        '  - ' + username + ': ' + userdesc.get('description', '') + '\n'
+                        for username, userdesc in self.registred_users.items()
+                    )
+                )
+            )
+
+    def __del__(self):
+        # This is the CLI end !
+        print("Done.")
+        self.server.send_signal(SIGINT)
+
+    def help_users(self):
+        print(
+            "Listes des utilisateurs enregistrés :\n{users}".format(
+                users=''.join(" - '" + user + "' : " + desc['name'] + "\n" for user, desc in self.registred_users.items())
+            )
+        )
+
+    def do_browse_as(self, line):
+        "Open a browser, connect as a registred user then wait"
+        if line in self.registred_users:
+            user = self.registred_users[line]
+            with sync_playwright() as p:
+                # Make sure to run headed.
+                browser = p.chromium.launch(headless=False)
+
+                # Setup context however you like.
+                context = browser.new_context()  # Pass any options
+                # context.route('**/*', lambda route: route.continue_())
+
+                # Pause the page, and start recording manually.
+                page = context.new_page()
+                page.goto("http://localhost:8123/")
+                page.get_by_label("Nom d’utilisateur :").fill(line)
+                page.get_by_label("Nom d’utilisateur :").press("Tab")
+                page.locator("#id_password").fill(user['password'])
+                page.get_by_role("button", name="Se connecter").click()
+                page.pause()
+        else:
+            print(
+                "Utilisateur inconnu : '{line}'. Liste des utilisateurs enregistrés :\n{users}".format(
+                    line=line,
+                    users=''.join(" - '" + user + "' : " + desc['name'] + "\n" for user, desc in self.registred_users.items()),
+                )
+            )
+        return False
+
+    def do_save_db(self, line):
+        "Save the tests database into backup.json.bz2"
+        run(['python', 'manage.py', 'backup'])
+
+    def do_EOF(self, line):
+        "Terminate the CLI"
+        return True
+
+    def do_quit(self, line):
+        "Terminate the CLI"
+        return True
+
+
+def main(args):
+    Cli(args).cmdloop()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description=_("A simple CLI to help building tests for the BiomAid project"))
+    args = parser.parse_args()
+    main(args)
