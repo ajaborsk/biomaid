2be3b174bbe4303f28e5d58836cc8b733b7538c4
alexandre <alexandre.jaborska@laposte.net>
2023-10-18 06:40:16 +0200

    more robust run-instance.sh

--- run-instance.sh
+++ run-instance.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 # Generate documentation (if needed)
-# ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/python runserver make_docs
+# ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry poetry env list --full-path | head -n1 | cut -d " " -f 1`bin/python runserver make_docs
+
+echo "using "`~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`
 
 # Deploy statics
-~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/python manage.py collectstatic --no-input
+`~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`/bin/python manage.py collectstatic --no-input
 
 # Run gunicorn workers
-exec ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry env list`/bin/gunicorn --workers 2 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
+exec `~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`/bin/gunicorn --workers 2 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
