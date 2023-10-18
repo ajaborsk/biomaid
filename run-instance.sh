6ec4342db86fc01224a8f9c2ea0c41a252bd9b62
alexandre <alexandre.jaborska@laposte.net>
2023-10-18 21:01:17 +0200

    Smarter run-instance.sh

--- run-instance.sh
+++ run-instance.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
+# VENV_BIN = `~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`/bin
+# MANAGE_PY_CMD = $VENV_BIN/python manage.py
+
 # Generate documentation (if needed)
 # ~/.cache/pypoetry/virtualenvs/`~/.local/bin/poetry poetry env list --full-path | head -n1 | cut -d " " -f 1`bin/python runserver make_docs
 
-echo "using "`~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`
+# echo "using python virtualenv: "$VENV_BIN
 
 # Deploy statics
-`~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`/bin/python manage.py collectstatic --no-input
+poetry run python manage.py collectstatic --no-input
 
 # Run gunicorn workers
-exec `~/.local/bin/poetry env list --full-path | head -n1 | cut -d " " -f 1`/bin/gunicorn --workers 2 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
+exec poetry run gunicorn --workers 2 --log-file ../log/gunicorn_django.log -b unix:$HOME/biomaid.sock dra.wsgi
